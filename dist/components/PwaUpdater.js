2613a569a7df207655bc59e9c882fc84ebc45bf5
LiChuanXD <billgatepls@hotmail.com>
2021-10-07 16:33:29 +0800

    testing

---
+++ dist/components/PwaUpdater.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+const PwaUpdater = () => {
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "PWA Updater"));
+};
+
+var _default = PwaUpdater;
+exports.default = _default;
---
+++ dist/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "PwaUpdater", {
+  enumerable: true,
+  get: function get() {
+    return _PwaUpdater.default;
+  }
+});
+Object.defineProperty(exports, "swRegister", {
+  enumerable: true,
+  get: function get() {
+    return _swRegister.default;
+  }
+});
+
+var _PwaUpdater = _interopRequireDefault(require("./components/PwaUpdater"));
+
+var _swRegister = _interopRequireDefault(require("./function/swRegister"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
